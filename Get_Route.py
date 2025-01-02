93832dcbeb9f55cb2e718eb406ddb32f42b21b6e
bishal <bishalbhaandari62@gmail.com>
2025-01-02 12:32:37 +0100

    script to get route specific route

--- Get_Route.py
+++ Get_Route.py
@@ -1,18 +1,18 @@
 from sumolib import checkBinary, net
 
-# Load the network file
+# network file
 net_file = "bamberg.net.xml"
 network = net.readNet(net_file)
 
-# Define the edges for your specific route
+# the edges for route
 route_edges = ["edge1", "edge2", "edge3", "edge4"]
 
-# Print details of the selected route
+# details of the selected route
 for edge_id in route_edges:
     edge = network.getEdge(edge_id)
     print(f"Edge: {edge_id}, Length: {edge.getLength()}")
 
-# Save route to a file
+# Save route to file
 with open("selected_routes.rou.xml", "w") as f:
     f.write('<routes>\n')
     f.write('<vType id="bus" accel="1.0" decel="4.0" length="12" maxSpeed="20" color="1,0,0"/>\n')
