9c8b9c9104940637833aecda79ec9b2ca57d69f9
bishal <bishalbhaandari62@gmail.com>
2025-01-03 18:51:30 +0100

    Get route full route from edges

--- Get_edges.py
+++ Get_edges.py
@@ -5,8 +5,8 @@ net_file = "bamberg.net.xml"
 network = net.readNet(net_file)
 
 # Start and end edges
-start_edge = "a"
-end_edge = "f"
+start_edge = "38033459"
+end_edge = "-199523015"
 
 # Find connected route
 route_edges = network.getShortestPath(network.getEdge(start_edge), network.getEdge(end_edge))
