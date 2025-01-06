654f2273f19eac35f3ca45516a4abb964e44a033
bishal <bishalbhaandari62@gmail.com>
2025-01-06 17:35:55 +0100

    Load file

--- Get_edges.py
+++ Get_edges.py
@@ -4,13 +4,9 @@ from sumolib import checkBinary, net
 net_file = "bamberg.net.xml"
 network = net.readNet(net_file)
 
-# Input list of edges
-edges_from_file = [
-    "-109507282", "-109829810#0", "-1130839432", "-1135618619", "-1135618621",
-    "-117367372#1", "-117367373#2", "-118319060#0", "-118321787#4",
-    # Add all edges from your file here
-]
-
+# Load edges from the file
+with open("Gaustadt.txt", "r") as file:
+    edges = [line.strip().replace("edge:", "") for line in file]
 
 # Function to get the ordered list of edges
 def sort_edges_serially(edge_list, net):
