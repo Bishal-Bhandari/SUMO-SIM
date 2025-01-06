a758665e996be1769d06e9f2e9f4e90526278c5a
bishal <bishalbhaandari62@gmail.com>
2025-01-06 15:47:44 +0100

    Get route full route from edges

--- Get_edges.py
+++ Get_edges.py
@@ -5,8 +5,8 @@ net_file = "bamberg.net.xml"
 network = net.readNet(net_file)
 
 # Start and end edges
-start_edge = "x"
-end_edge = "y"
+start_edge = "-109507282"
+end_edge = "-109829810#0"
 
 # Find connected route
 try:
@@ -17,13 +17,4 @@ except Exception as e:
     print(f"Error finding route: {e}")
     exit()
 
-# Save the route as a .rou.xml file
-output_file = "calculated_routes.rou.xml"
-with open(output_file, "w") as f:
-    f.write('<routes>\n')
-    f.write('<vType id="bus" accel="1.0" decel="4.0" length="12" maxSpeed="20" color="1,0,0"/>\n')
-    f.write(f'<route id="calculatedRoute" edges="{" ".join(edge_ids)}"/>\n')
-    f.write('<vehicle id="bus_1" type="bus" route="calculatedRoute" depart="0" color="1,0,0"/>\n')
-    f.write('</routes>\n')
 
-print(f"Route saved to {output_file}")
